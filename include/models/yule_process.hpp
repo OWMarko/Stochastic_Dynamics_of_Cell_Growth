c020e3bb4327aba56faa0cd9397c81de00a30ae2
Marko <sinadinovic.marko.professionnel@gmail.com>
2026-02-05 20:15:42 +0000

    Update yule_process.hpp
    Removed comments and documentation for the Yule process simulation.

--- include/models/yule_process.hpp
+++ include/models/yule_process.hpp
@@ -9,14 +9,8 @@ namespace YuleModel {
         std::vector<int> population;
     };
 
-    /**
-     * @brief Simule un processus de Yule (Explosion démographique)
-     * Utilise le Lemme du Minimum (Gillespie)
-     */
     PopulationResult run_population_simulation(double lambda, double t_max);
 
-    // --- POUR LE BENCHMARK ---
-    // On veut la population finale de M expériences différentes
     std::vector<int> run_batch_serial(double lambda, double t_max, int M_simulations);
     std::vector<int> run_batch_parallel(double lambda, double t_max, int M_simulations);
 }
